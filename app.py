5815cf6ad588ae05dea452a60fd045192db2c9b0
NoMadBucky <blonien@uwalumni.com>
2017-06-08 17:25:29 -0500

    copy from web materials

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+import mod_wsgi.server
+
+mod_wsgi.server.start(
+  '--log-to-terminal',
+  '--port', '8080',
+  '--trust-proxy-header', 'X-Forwarded-For',
+  '--trust-proxy-header', 'X-Forwarded-Port',
+  '--trust-proxy-header', 'X-Forwarded-Proto',
+  '--processes', os.environ.get('MOD_WSGI_PROCESSES', '1'),
+  '--threads', os.environ.get('MOD_WSGI_THREADS', '5'),
+  '--url-alias', '/static/', './static/',
+  '--application-type', 'module',
+  '--entry-point', 'demo.wsgi',
+)
